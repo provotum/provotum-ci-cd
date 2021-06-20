764426cee92949d6e22ba8977df4da000c754b04
Roger Staubli <roger.staubli@uzh.ch>
2021-06-20 23:40:21 +0200

    change ttl

--- terraform/provotum-v3-mn/dns.tf
+++ terraform/provotum-v3-mn/dns.tf
@@ -2,12 +2,14 @@ resource "digitalocean_record" "server" {
   domain = data.digitalocean_domain.default.name
   type   = "A"
   name   = var.subdomain
+  ttl    = 10
   value  = element(digitalocean_droplet.server,0).ipv4_address
 }
 resource "digitalocean_record" "server_sub" {
   domain = data.digitalocean_domain.default.name
   type   = "A"
   name   = "*.${var.subdomain}"
+  ttl    = 10
   value  = element(digitalocean_droplet.server,0).ipv4_address
 }
 resource "digitalocean_record" "sealer" {
@@ -15,5 +17,6 @@ resource "digitalocean_record" "sealer" {
   domain = data.digitalocean_domain.default.name
   type   = "A"
   name   = "sealer${count.index}.${var.subdomain}"
+  ttl    = 10
   value  = element(digitalocean_droplet.sealer, count.index).ipv4_address
 }
