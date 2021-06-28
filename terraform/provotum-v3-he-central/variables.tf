a5f6b5279aedc810ae25c64a3cc492b68df13479
Roger Staubli <roger.staubli@uzh.ch>
2021-06-28 16:22:33 +0200

    add variables to provotum v3 he central

--- terraform/provotum-v3-he-central/variables.tf
+++ terraform/provotum-v3-he-central/variables.tf
@@ -1,5 +1,8 @@
 variable "do_token" {}
 variable "pvt_key" {}
+variable "docker_registry" {}
+variable "docker_registry_username" {}
+variable "docker_registry_password" {}
 variable "subdomain_server" {
   type = string
   default = "v3-he-central"
