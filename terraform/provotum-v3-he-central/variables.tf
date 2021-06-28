574c4876f4d340f49a0531d4ceb3cea820e1fa7f
Roger Staubli <roger.staubli@uzh.ch>
2021-06-28 16:23:48 +0200

    change to subdomain name for variable

--- terraform/provotum-v3-he-central/variables.tf
+++ terraform/provotum-v3-he-central/variables.tf
@@ -3,7 +3,7 @@ variable "pvt_key" {}
 variable "docker_registry" {}
 variable "docker_registry_username" {}
 variable "docker_registry_password" {}
-variable "subdomain_server" {
+variable "subdomain" {
   type = string
   default = "v3-he-central"
 }
